6d4fc53127b959245dba963867e92710e41bd9da
Techman <wizmar.tyler@gmail.com>
2019-01-28 14:52:47 -0600

    Changed console.log's to webpage information.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,26 +27,33 @@ $("#find-video").on("click", function (event) {
    };
 
    EVDB.API.call("/events/search", oArgs, function (oData) {
-      console.log(oData);
       const {
          event
       } = oData.events;
-
-      for (i = 0; i < event.length; i++) {
-         console.log(event[i].start_time);
-         console.log(event[i].venue_name);
-         if (event[i].performers === null) {
-            console.log("More Info at: " + event[i].url);
-         } else if (event[i].performers.performer.length > 1) {
-
-
-            for (j = 0; j < event[i].performers.performer.length; j++) {
-
-               console.log(event[i].performers.performer[j].name);
+      $("#emptyDiv").empty();
+      for (i = 0; i < event.length; i++) 
+      {
+         var newDiv = $("<div>");
+         newDiv.text("Starts At: " + event[i].start_time + " ");
+         newDiv.append("Venue Name: " + event[i].venue_name + " ");
+         if (event[i].performers === null) 
+         {
+            newDiv.append("Performing: ")
+            $("<a>", {href: event[i].url}).append("More Info Here").appendTo(newDiv);
+         } 
+         else if (event[i].performers.performer.length > 1) 
+         {
+            newDiv.append("Performing: ")
+            for (j = 0; j < event[i].performers.performer.length; j++) 
+            {
+               newDiv.append(event[i].performers.performer[j].name + " ");
             }
-         } else {
-            console.log(event[i].performers.performer.name);
+         } 
+         else 
+         {
+            newDiv.append("Performer: " + event[i].performers.performer.name);
          }
+         $("#emptyDiv").append(newDiv);
       }
       // Note: this relies on the custom toString() methods below
 
